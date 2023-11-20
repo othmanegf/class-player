e15d1fc9d792740554804a1f316f48b852fd4d5c
othmane <147095181+othmanegf@users.noreply.github.com>
2023-11-20 18:49:32 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+
+        Player firstPlayer = new Player();
+        firstPlayer.name = "messi";
+        firstPlayer .age=36 ;
+        firstPlayer.rank = 99;
+        firstPlayer.pass();
+        Player secondPlayer = new Player();
+        secondPlayer.name = "neymar";
+        secondPlayer.jump();
+
+    }
+}
