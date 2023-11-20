97e83c2342d8b0f20329d33b29f58d5020da5de1
othmane <147095181+othmanegf@users.noreply.github.com>
2023-11-20 19:05:17 +0100

    Create player.java

---
+++ player.java
@@ -0,0 +1,14 @@
+class Player {
+    String name;
+    short age;
+    short rank;
+    void pass() {
+        System.out.println(name + " passing!");
+    }
+    void shoot() {
+        System.out.println(name + " shooting!");
+    }
+    void jump() {
+        System.out.println(name + " jumping!");
+    }
+}
